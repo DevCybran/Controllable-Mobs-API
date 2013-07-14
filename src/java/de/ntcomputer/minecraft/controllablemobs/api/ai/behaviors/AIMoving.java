6e0470ad3f2e66755e2c54ddf810285a1bb8ac2b
DevCybran <public@ntcomputer.de>
2013-07-14 18:02:27 +0200

    changed AIMoving constructor visibility to protected

--- src/java/de/ntcomputer/minecraft/controllablemobs/api/ai/behaviors/AIMoving.java
+++ src/java/de/ntcomputer/minecraft/controllablemobs/api/ai/behaviors/AIMoving.java
@@ -2,10 +2,17 @@ package de.ntcomputer.minecraft.controllablemobs.api.ai.behaviors;
 
 import org.bukkit.entity.LivingEntity;
 
+/**
+ * Abstract class which defines an AI behavior that requires the entity to move.
+ * 
+ * @author Cybran
+ * @version v4
+ * 
+ */
 public abstract class AIMoving<E extends LivingEntity> extends AIBehavior<E> {
 	protected final double movementSpeedMultiplicator;
 
-	public AIMoving(int priority, double movementSpeedMultiplicator) {
+	protected AIMoving(int priority, double movementSpeedMultiplicator) {
 		super(priority);
 		this.movementSpeedMultiplicator = movementSpeedMultiplicator;
 	}
