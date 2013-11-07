294c7a965e1d2c72c37995138aaff2f35b0033b7
DevCybran <commit@ntcomputer.de>
2013-11-07 16:20:12 +0100

    re-ordered attributes

--- src/java/de/ntcomputer/minecraft/controllablemobs/implementation/nativeinterfaces/server/NmsGenericAttributes.java
+++ src/java/de/ntcomputer/minecraft/controllablemobs/implementation/nativeinterfaces/server/NmsGenericAttributes.java
@@ -6,12 +6,10 @@ import de.ntcomputer.minecraft.controllablemobs.implementation.nativeinterfaces.
 
 public final class NmsGenericAttributes {
 	// look at the string description in GenericAttributes class on update
+	public final NativeStaticFieldObject<GenericAttributes,IAttribute> STATIC_FIELD_MAXHEALTH = new NativeStaticFieldObject<GenericAttributes,IAttribute>(GenericAttributes.class,"a");
+	public final NativeStaticFieldObject<GenericAttributes,IAttribute> STATIC_FIELD_FOLLOWRANGE = new NativeStaticFieldObject<GenericAttributes,IAttribute>(GenericAttributes.class,"b");
+	public final NativeStaticFieldObject<GenericAttributes,IAttribute> STATIC_FIELD_KNOCKBACKRESISTANCE = new NativeStaticFieldObject<GenericAttributes,IAttribute>(GenericAttributes.class,"c");
 	public final NativeStaticFieldObject<GenericAttributes,IAttribute> STATIC_FIELD_MOVEMENTSPEED = new NativeStaticFieldObject<GenericAttributes,IAttribute>(GenericAttributes.class,"d");
 	public final NativeStaticFieldObject<GenericAttributes,IAttribute> STATIC_FIELD_ATTACKDAMAGE = new NativeStaticFieldObject<GenericAttributes,IAttribute>(GenericAttributes.class,"e");
-	public final NativeStaticFieldObject<GenericAttributes,IAttribute> STATIC_FIELD_MAXHEALTH = new NativeStaticFieldObject<GenericAttributes,IAttribute>(GenericAttributes.class,"a");
-	public final NativeStaticFieldObject<GenericAttributes,IAttribute> STATIC_FIELD_KNOCKBACKRESISTANCE = new NativeStaticFieldObject<GenericAttributes,IAttribute>(GenericAttributes.class,"c");
-	public final NativeStaticFieldObject<GenericAttributes,IAttribute> STATIC_FIELD_FOLLOWRANGE = new NativeStaticFieldObject<GenericAttributes,IAttribute>(GenericAttributes.class,"b");
-	
-	
 	
 }
